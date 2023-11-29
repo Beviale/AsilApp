5c9aa9f5f354660396f3e9a8d497e5d1a0b53cd4
mattiacolucci <mattiacolucci60@gmail.com>
2023-11-29 20:14:50 +0100

    db

--- app/src/main/java/dao/Dao.java
+++ app/src/main/java/dao/Dao.java
@@ -14,6 +14,7 @@ import java.util.Map;
 
 public class Dao {
     private FirebaseFirestore db;
+    private String email="gliasphaltatori@gamil.com";
 
     public Dao() {
         db= FirebaseFirestore.getInstance();
