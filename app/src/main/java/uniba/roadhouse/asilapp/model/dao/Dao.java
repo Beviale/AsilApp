7bf7ddc3107f64b062b0f6e12379655fd3d4b74b
mattiacolucci <mattiacolucci60@gmail.com>
2023-12-01 14:41:07 +0100

    registrazione utente db

--- app/src/main/java/uniba/roadhouse/asilapp/model/dao/Dao.java
+++ app/src/main/java/uniba/roadhouse/asilapp/model/dao/Dao.java
@@ -13,6 +13,10 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,6 +25,9 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.regex.Pattern;
+
+import uniba.roadhouse.asilapp.R;
 
 public class Dao {
     private FirebaseFirestore db;
@@ -68,5 +75,63 @@ public class Dao {
         return nomiResidenze;
     }
 
-    public 
+    public String registerUser(String username, String password, String nome, String cognome, String cittadinanza, String sesso, String paese, String residenza){
+
+        //verifico se esiste un utente con lo username dell'utente che si vuole registrare
+        Task<QuerySnapshot> query=db.collection("users").whereEqualTo("username",username).get();
+
+        while (!query.isComplete()) {
+            //attenendo che la funzione asincrona chaimata termini la sua computazione
+        }
+
+        //quando la query è completata vedo se esiste un utente con lo username scelto
+        if(query.getResult().size()>0){
+            return String.valueOf(R.string.userAlreadyExists);
+        }
+
+        //verifico che la password rispetta i criteri previsti
+        //password deve avere almeno 8 caratteri, almeno una lettera maiuscola, un carattere speciale e un numero
+        if(!Pattern.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",password)){
+            return String.valueOf(R.string.passwordDoNotMatchRegEx);
+        }
+
+        //se va tutto bene faccio l'hash della password
+        //faccio l'hash della password
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        byte[] hashedPassword;
+        random.nextBytes(salt);
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+            md.update(salt);
+            hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        //se va tutto bene, creo la mappa che rappresenta i dati dell'utente
+        Map<String,Object> user=new HashMap<>();
+        user.put("username",username);
+        user.put("password",hashedPassword);
+        user.put("sesso",sesso);
+        user.put("nome",nome);
+        user.put("cognome",cognome);
+        user.put("paeseNativo",paese);
+        user.put("cittadinanza",cittadinanza);
+
+        //aggiungo l'utente al db
+        Task<DocumentReference> addToDb=db.collection("users").add(user);
+
+        while (!addToDb.isComplete()) {
+            //attenendo che la funzione asincrona chaimata termini la sua computazione
+        }
+
+        if(!addToDb.isSuccessful()){
+            return String.valueOf(R.string.insertUserFailed);
+        }
+
+        //se l'inserimento è avvenuto con successo ritono il messaggio
+        return String.valueOf(R.string.registrationComplete);
+    }
 }
