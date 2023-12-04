148e78f6abf2f4aa1b3cb0e738e51512060555a5
Alessandro Bevilacqua <beviale@outlook.it>
2023-12-04 18:59:10 +0100

    Home

--- app/src/main/java/uniba/roadhouse/asilapp/view/home/HomeActivity.java
+++ app/src/main/java/uniba/roadhouse/asilapp/view/home/HomeActivity.java
@@ -1,16 +1,50 @@
 package uniba.roadhouse.asilapp.view.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
 
 import uniba.roadhouse.asilapp.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    ImageView toolBarDownIconMedicalParameters;
+    Boolean medicalParamtersOpen=false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.home_activity);
+        Window window = this.getWindow();
+        window.setStatusBarColor(getColor(R.color.appBarColor));
+        toolBarDownIconMedicalParameters = findViewById(R.id.toolBarDownIconMedicalParameters);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        toolBarDownIconMedicalParameters.setOnClickListener(v->openMedicalParamters());
+    }
+
+    private void openMedicalParamters()
+    {
+        if(medicalParamtersOpen==false)
+        {
+            toolBarDownIconMedicalParameters.setImageResource(R.mipmap.heart_pressed);
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.homeContainerView, MedicalParametersFragment.class, null);
+            fragmentTransaction.commit();
+            medicalParamtersOpen=true;
+        }
+        else
+        {
+            toolBarDownIconMedicalParameters.setImageResource(R.mipmap.heart);
+        }
     }
 }
