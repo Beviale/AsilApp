036cef64316fc468ab298c4fb0c5478fcc5110dd
mattiacolucci <mattiacolucci60@gmail.com>
2023-12-05 10:19:58 +0100

    home

--- app/src/main/java/uniba/roadhouse/asilapp/view/home/HomeActivity.java
+++ app/src/main/java/uniba/roadhouse/asilapp/view/home/HomeActivity.java
@@ -11,19 +11,34 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 
 import com.google.android.material.datepicker.MaterialCalendar;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import uniba.roadhouse.asilapp.R;
 
 public class HomeActivity extends AppCompatActivity {
-
-    ImageView toolBarDownIconMedicalParameters;
-    int currentSectionOpen=-1;
-    Menu menuBottomBar;
-    Toolbar bottomBar;
+    Map<String,Integer> screenIcons=new HashMap<String, Integer>() {{
+        put("user", R.id.user_icon);
+        put("health", R.id.health_icon);
+        put("health box",R.id.health_box_icon);
+    }};
+    Map<String,Integer> screenIconsBg=new HashMap<String, Integer>() {{
+        put("user", R.id.user_icon_bg);
+        put("health", R.id.health_icon_bg);
+        put("health box",R.id.health_box_icon_bg);
+    }};
+    Map<String,Class> screenFragments=new HashMap<String, Class>() {{
+        put("user", UserProfileFragment.class);
+        put("health", MedicalParametersFragment.class);
+        put("health box", HealthBoxFragment.class);
+    }};
+    String currentSectionOpen="user";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,50 +46,30 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.home_activity);
         Window window = this.getWindow();
         window.setStatusBarColor(getColor(R.color.appBarColor));
-
-        bottomBar=findViewById(R.id.toolbarHomeActivityDown);
-        setSupportActionBar(bottomBar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        //toolbar.setNavigationIcon(R.drawable.ic_toolbar);
-        bottomBar.setTitle("");
-        bottomBar.setSubtitle("");
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.botton_menu_home,menu);
-        menuBottomBar=menu;
-        return true;
+    protected void onStart() {
+        super.onStart();
+        findViewById(R.id.user_icon_layout).setOnClickListener(v->changeScreen("user"));
+        findViewById(R.id.health_box_icon_layout).setOnClickListener(v->changeScreen("health box"));
+        findViewById(R.id.health_icon_layout).setOnClickListener(v->changeScreen("health"));
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        super.onOptionsItemSelected(item);
-        int id=item.getItemId();
-
-        //se ho cliccato sul menu della salute, lancio il fragment ed illumino l'icona nel caso in cui non sia quela attualmente attiva
-        if(id==R.id.healthMenuIcon){
-            openHomeScreen(item,MedicalParametersFragment.class,R.mipmap.heart_pressed);
-        }
-
-        return true;
-
-    }
-
-    private void openHomeScreen(MenuItem item, Class fragmentToOpen, int icon_active){
+    private void changeScreen(String screen){
         //apro il fragment che inidca la sezione cliccata da aprire
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.homeContainerView, fragmentToOpen, null);
+        fragmentTransaction.replace(R.id.homeContainerView, screenFragments.get(screen), null);
         fragmentTransaction.commit();
 
         //se la schermata attuale non è quella che sto aprendo, allora imposto l'icona della schermata da aprire
         //come schermata attiva (è illuminata)
-        if(currentSectionOpen!=item.getItemId()){
-            item.setIcon(icon_active);
+        if(currentSectionOpen!=screen){
+            findViewById(screenIconsBg.get(screen)).setVisibility(View.VISIBLE);
+            findViewById(screenIconsBg.get(currentSectionOpen)).setVisibility(View.INVISIBLE);
         }
 
-        currentSectionOpen=item.getItemId();
+        currentSectionOpen=screen;
     }
 }
