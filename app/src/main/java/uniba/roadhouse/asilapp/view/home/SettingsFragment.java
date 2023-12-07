9971ec87eff929536b7dc3ed2aa82514ebbb43c2
Alessandro Bevilacqua <beviale@outlook.it>
2023-12-08 00:02:37 +0100

    Setting exit Account

--- app/src/main/java/uniba/roadhouse/asilapp/view/home/SettingsFragment.java
+++ app/src/main/java/uniba/roadhouse/asilapp/view/home/SettingsFragment.java
@@ -3,6 +3,7 @@ package uniba.roadhouse.asilapp.view.home;
 import static android.content.Context.MODE_APPEND;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
@@ -10,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -31,6 +33,7 @@ import java.util.concurrent.CompletableFuture;
 import uniba.roadhouse.asilapp.R;
 import uniba.roadhouse.asilapp.model.dao.Access;
 import uniba.roadhouse.asilapp.model.dao.Dao;
+import uniba.roadhouse.asilapp.view.signinSignup.SigninSingupActivity;
 
 
 public class SettingsFragment extends Fragment {
@@ -128,6 +131,8 @@ public class SettingsFragment extends Fragment {
 
     private void exitAccount()
     {
-
+        Dao.logOutUser(getActivity());
+        Intent intent = new Intent(getActivity(), SigninSingupActivity.class);
+        startActivity(intent);
     }
 }
